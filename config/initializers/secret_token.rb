14dd62fb11c469d5af9009726a7fcaae31a8c520
Mumo Carlos <mumo.crls@gmail.com>
2014-03-07 10:38:41 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MumocsTesting::Application.config.secret_key_base = '7c4de72a0b221b7bb13d5bedc200bf498de7a86de840b9ff374c7231b09aa17a691f9b4dcbd7a862b866a41843729df14336116627aca91a374fb427759ddcdb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MumocsTesting::Application.config.session_store :cookie_store, key: '_mumocs-testing_session'
